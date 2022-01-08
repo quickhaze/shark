c92dc39e7f3bf8b38bf82c4c6bb5c2947d4c0cce
Yuneesh <yuneesh.uddeshiya@gmail.com>
2022-01-09 02:33:17 +0530

    ran test case

--- whale/models.py
+++ whale/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Animal(models.Model):
     name = models.CharField(max_length=100)
     sound = models.CharField(max_length=100)
+
+
+    def speak(self):
+        return f'The {self.name} says "{self.sound}"'
