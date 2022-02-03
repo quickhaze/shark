9607b223b033a36ad3aa7a804216d3dadb94cf21
Yuneesh <yuneesh.uddeshiya@gmail.com>
2022-02-03 17:16:26 +0530

    base url root view

--- root/views.py
+++ root/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views import View
 
 
-BASE_URL = "THIS IS TEST"
+BASE_URL = "https://github.com/quickhaze/shark/pulls"
 
 
 # Create your views here.
@@ -17,4 +17,3 @@ def hello_view(request):
 class IndexView(View):
     def get(self, request, *args, **kwrags):
         return HttpResponse("This is  index")
-    
