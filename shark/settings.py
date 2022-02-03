3fc8d75ffa6217466125cf6335571dfc9721c813
Akash <akash@nitrx.com>
2022-02-03 18:21:16 +0530

    Setting

--- shark/settings.py
+++ shark/settings.py
@@ -121,14 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-<<<<<<< HEAD
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-=======
-STATIC_URL = "/static/"
->>>>>>> c892de625e93deb6a84eeca78f5f235f03dd2741
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
