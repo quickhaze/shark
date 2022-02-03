0354499677fafd68ba0b3e99dfc3ebe8d1a2cf06
Yuneesh <yuneesh.uddeshiya@gmail.com>
2022-02-03 16:52:56 +0530

    Added root index view

--- root/views.py
+++ root/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 
 
 def hello_view(request):
     return HttpResponse("Hello")
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwrags):
+        return HttpResponse("This is  index")
