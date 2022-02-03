3dd0859b723ee2e2f0f91d8192fbc41b9bd7a193
Akash <akash@nitrx.com>
2022-02-03 19:11:29 +0530

    HomeView url added

--- root/views.py
+++ root/views.py
@@ -9,19 +9,24 @@ BASE_URL = "127.0.0.1:8000/root/"
 def administration(request):
     return render(request, "homepage.html")
 
+
 def hii(request):
     return HttpResponse("this hii virew")
 
+
 def hello_view(request):
     return HttpResponse("Hello")
+
 
 class IndexView(View):
     def get(self, request, *args, **kwrags):
         return HttpResponse("This is  index")
 
+
 class HomeView(View):
     def get(request):
         return HttpResponse("<h1>Hello All</h1>")
+
 
 def arm(request):
     return HttpResponse("try this is")
