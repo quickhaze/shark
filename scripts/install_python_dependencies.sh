2992224f6fa9a0e100a6983166d508cdc9e567a8
Yuneesh <yuneesh.uddeshiya@gmail.com>
2022-02-20 03:58:42 +0530

    updated path

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-chown ubuntu:ubuntu /home/ubuntu/tutor/shark
-python -m venv /home/ubuntu/tutor/env
-chown ubuntu:ubuntu /home/ubuntu/tutor/env
-chown ubuntu:ubuntu /home/ubuntu/tutor/env/*
-source /home/ubuntu/tutor/env/bin/activate
-pip install -r /home/ubuntu/tutor/shark/requirements.txt
+chown ubuntu:ubuntu /home/ubuntu/tt1/shark
+python -m venv /home/ubuntu/tt1/env
+chown ubuntu:ubuntu /home/ubuntu/tt1/env
+chown ubuntu:ubuntu /home/ubuntu/tt1/env/*
+source /home/ubuntu/tt1/env/bin/activate
+pip install -r /home/ubuntu/tt1/shark/requirements.txt
